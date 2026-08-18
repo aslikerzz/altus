a6f566e386b9fb313e93d794a309b3fb66b4e0b0
aslik <94638851+aslikerzz@users.noreply.github.com>
2026-08-18 16:29:30 +0200

    Add French (fr) translation and update package resolutions

--- src/i18n/langauges.config.ts
+++ src/i18n/langauges.config.ts
@@ -1,3 +1,3 @@
-export const languages = ["en", "es", "hi", "it", "pt-br", "de", "tr", "ko"] as const;
+export const languages = ["en", "es", "hi", "it", "pt-br", "de", "tr", "ko", "fr"] as const;
 export type Language = (typeof languages)[number];
 export const fallbackLanguage = "en";
